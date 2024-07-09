7e9daeaeec325b56dc6d43126127ea4d62dd0796
Rola <147377687+RolaDinnawi@users.noreply.github.com>
2024-07-10 01:18:21 +0300

    Update Song.java

--- allears/Song.java
+++ allears/Song.java
@@ -1,6 +1,9 @@
 package com.example.allears;
 
-public class Song {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Song implements Parcelable {
     private String name;
     private String artist;
     private int imageResourceId;
@@ -9,6 +12,36 @@ public class Song {
         this.name = name;
         this.artist = artist;
         this.imageResourceId = imageResourceId;
+    }
+
+    protected Song(Parcel in) {
+        name = in.readString();
+        artist = in.readString();
+        imageResourceId = in.readInt();
+    }
+
+    public static final Creator<Song> CREATOR = new Creator<Song>() {
+        @Override
+        public Song createFromParcel(Parcel in) {
+            return new Song(in);
+        }
+
+        @Override
+        public Song[] newArray(int size) {
+            return new Song[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(name);
+        dest.writeString(artist);
+        dest.writeInt(imageResourceId);
     }
 
     public String getName() {
